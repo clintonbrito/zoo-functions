85c881ac888c2228efbc41ef9edc84fedae846f7
Clinton Brito <clintoncsb@gmail.com>
2023-03-13 20:39:15 -0300

    Resolvendo o 3o requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const dataEmployees = data.employees;
+
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  } if (data.employees.find((employee) => employee.firstName === employeeName)) {
+    return data.employees.find((employee) => employee.firstName === employeeName);
+  }
+  return data.employees.find((employee) => employee.lastName === employeeName);
 };
 
 module.exports = getEmployeeByName;
