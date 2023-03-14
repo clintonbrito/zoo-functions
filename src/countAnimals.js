6512444dd138eba05c77909d1ad1372ae925ebb3
Clinton Brito <clintoncsb@gmail.com>
2023-03-14 12:21:30 -0300

    Resolvendo o 4o requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { name, popularity } = data.species;
+const nameAndPopularity = `${name}: ${popularity}`;
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    return {
+      ${name}: ${popularity}
+    };
+  }
+  return nameAndPopularity;
 };
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
+const dataEmployees = data.employees;
+
+const isManager = (managerId) => {
+  const gerente = data.employees.map((employee) =>
+    employee.managers.some((manager) => manager === managerId));
+  return gerente.includes(true);
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    // A constante abaixo busca filtrar (filter) se tem algum (some) empregado que tenha como gerente o gerente que foi dado como parâmetro (managerId);
+    const empregados = data.employees
+      .filter((employee) =>
+        employee.managers.some((manager) => managerId === manager));
+    // A constante abaixo busca mapear (map) dentro da const 'empregados' as pessoas colaboradoras do respectivo gerente que foi dado como parâmetro (managerId) e "cuspir" em forma de string o nome e sobrenome dessas pessoas;
+    const employeesNames = empregados.map((item) => `${item.firstName} ${item.lastName}`);
+    return employeesNames;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
+
+// Neste requisito tive o auxílio na monitoria do Summer Marcell Pena no dia 14/03
 
 module.exports = { isManager, getRelatedEmployees };
