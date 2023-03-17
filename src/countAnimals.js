4ae98cc2ebbf48daddb35c2e3ac5ededd7fcbf04
Clinton Brito <clintoncsb@gmail.com>
2023-03-16 23:01:13 -0300

    Resolvendo o 5o requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,15 +1,26 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-// const { name, popularity } = data.species;
-// const nameAndPopularity = `${name}: ${popularity}`;
-
 const countAnimals = (animal) => {
-  // if (animal === undefined) {
-  //   return {
-  //     ${name}: ${popularity}
-  //   };
-  // }
-  // return nameAndPopularity;
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+
+  const bicho = species.find((specie) => animal.species === specie.name);
+
+  if (animal.species && animal.sex === undefined) {
+    return bicho.residents.length;
+  }
+
+  const bichosPorSexo = bicho.residents.filter((resident) => resident.sex === animal.sex);
+  return bichosPorSexo.length;
 };
+
+countAnimals();
+
+// Para esse requisito, tive o auxílio fundamental da monitoria notura do Victor Matias no dia 16/03.
 
 module.exports = countAnimals;
