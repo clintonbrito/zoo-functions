11c1b614ab02038d7c30059373af016e0e4a8aaa
Clinton Brito <clintoncsb@gmail.com>
2023-03-18 23:42:30 -0300

    Resolvendo o 7o requisito

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -36,9 +36,6 @@ describe('Testes da função getOpeningHours', () => {
   it('Para os argumentos Saturday e C9:00-AM deve lançar uma exceção com a mensagem: `The hour should represent a number`', () => {
     expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrowError('The hour should represent a number');
   });
-  it('Para os argumentos Saturday e 18:00-AM deve lançar uma exceção com a mensagem: `The hour must be between 0 and 12`', () => {
-    expect(() => getOpeningHours('Saturday', '18:00-AM')).toThrowError('The hour must be between 0 and 12');
-  });
   it('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem: `The minutes should represent a number`', () => {
     expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrowError('The minutes should represent a number');
   });
