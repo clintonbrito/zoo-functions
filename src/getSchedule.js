ed10a2a5987c32898c7cb581406d30a14a216bde
Clinton Brito <clintoncsb@gmail.com>
2023-03-21 01:29:27 -0300

    Resolvendo o 9o requisito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
+const openingHours = Object.keys(hours);
+const createObj = () => {
+  const reduceObj = openingHours.reduce((acc, curr) => {
+    acc[curr] = {
+      officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
+      exhibition: species
+        .filter((bicho) => bicho.availability
+          .includes(curr))
+        .map(({ name }) => name),
+    };
+    return acc;
+  }, {});
+  reduceObj.Monday = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return reduceObj;
+};
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (species.some(({ name }) => name === scheduleTarget)) {
+    return species.find(({ name }) => name === scheduleTarget).availability;
+  } if (openingHours.includes(scheduleTarget)) {
+    return { [scheduleTarget]: createObj()[scheduleTarget] };
+  }
+  return createObj();
 };
 
 module.exports = getSchedule;
