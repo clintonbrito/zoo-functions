b15a17582f4e1359b86821b558875470bc5a3ae6
Clinton Brito <clintoncsb@gmail.com>
2023-03-21 02:02:21 -0300

    Resolvendo o 10o requisito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const findAnimalId = employees.find((idEmployee) => idEmployee.id === id).responsibleFor[0];
+  const findOldestAnimal = species
+    .find((ageAnimal) => ageAnimal.id === findAnimalId).residents
+    .sort((a, b) => b.age - a.age);
+  const result = findOldestAnimal.find(({ age }) => age);
+  return Object.values(result);
 };
 
 module.exports = getOldestFromFirstSpecies;
